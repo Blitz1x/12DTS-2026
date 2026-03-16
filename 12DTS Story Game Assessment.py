095791528c8fca2654013ba3aef59857c6ecd910
Blitz1x <23144@wc.school.nz>
2026-03-16 15:07:43 +1300

    Adding hand evaluation such as, pairs, threes, fours, straights, flushes

--- 12DTS Story Game Assessment.py
+++ 12DTS Story Game Assessment.py
@@ -10,7 +10,13 @@ import time
 #These are the variables for the card deck, the four suits and all the values in a normal game of texas holdem.
 suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 cards_list = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+community_cards
+player_hand = []
 deck = []
+values = []
+suits = []
+counts = {}
+cards = player_hand + community_cards
 
 card_values = {
     "2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,
@@ -28,7 +34,6 @@ def shuffle_deck(deck): # This code shuffles the deck so that every card is rand
     random.shuffle(deck)
 
 def deal_player():#Deals players cards.
-    player_hand = []
     for i in range(2):
         player_hand.append(deck.pop())
     return player_hand
@@ -36,13 +41,13 @@ def deal_player():#Deals players cards.
 def deal_flop(deck): #This is the code for the community cards, the flop, turn, and river.
     return [deck.pop(), deck.pop(), deck.pop()]
 
-def deal_turn(deck):
+def deal_turn(deck): #Deal Turn
     return [deck.pop()]
 
-def deal_river(deck):
+def deal_river(deck): #Deal River
     return [deck.pop()]
 
-def player_action():
+def player_action(): #What the player would like to do
     print("\nChoose Action")
     print("1. Check/Call")
     print("2. Raise")
@@ -51,10 +56,62 @@ def player_action():
     action = input("> ")
     return action
 
-def get_value(card):
+def get_value(card): #Get the value of the cards
     return card_values[card.split("")[0]]
-def get_suit(card):
+def get_suit(card): #Get the suit of the card
     return card.split("")[2]
+
+def get_values(cards):
+    for card in cards:
+        values.append(get_value(card))
+    return values
+
+def get_suits(cards):
+    for card in cards:
+        suits.append(get_suit(card))
+    return suits
+
+def count_values(values): #To see doubles triples etc.
+    for v in values:
+        if v in counts:
+            counts[v] += 1
+        else:
+            counts = 1
+    return counts
+
+def is_flush(suits): #To see if you have a flush
+    for suit in suits:
+        if suits.count(suit) >= 5:
+            return True
+    return False
+
+def is_straight(values): # See if you have a straight
+    sorted_values = sorted(set(values))
+    if len(sorted_values) < 5:
+        return False
+    for i in range (len(values)-4) #Normal Straight 2,3,4,5,6 etc
+        if values[i+4] - values[i] == 4:
+            return True
+    if set([14,2,3,4,5]): #For low straight Ace,2,3,4,5
+        return True
+    return False
+
+def evaluate_hand(hand, community):
+    values = get_values(cards)
+    suits = get_suits(cards)
+    counts = count_values(values)
+
+    pairs = 0
+    three = False
+    four = False
+
+    for c in counts.values():
+        if c == 2:
+            pairs += 1
+        elif c == 3:
+            three = True
+        elif c == 4:
+            four = True
 
 
 # ---- Loop ----
